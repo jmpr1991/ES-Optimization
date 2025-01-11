d5399f466e4bbb771df41b3324e61153abebaee0
jmpr1 <jmpr1991@gmail.com>
2025-01-11 14:03:54 +0100

    survival_selection finalized

--- opt_main.py
+++ opt_main.py
@@ -10,21 +10,37 @@ import numpy as np
 
 def main():
 
+    # Initialization of variables
+    best_adaptation_value = np.full(shape=constants.N_GENERATIONS, fill_value=np.nan)
+
     # Initialization of the population
     parent_population = initialization.initialization_function()
 
-    # Parent selection and recombination
-    offspring_population = Recombination.recombination_function(parent_population)
+    gen = 0
+    while gen < constants.N_GENERATIONS:
 
-    # Mutation
-    mutated_population = mutation.mutation_function(offspring_population)
+        # Parent selection and recombination
+        offspring_population = Recombination.recombination_function(parent_population)
 
-    # Survival selection
-    survival_population = survival_selection.survival_selection_function(mutated_population, parent_population)
+        # Mutation
+        mutated_population = mutation.mutation_function(offspring_population)
+
+        # Survival selection
+        parent_population, sorted_adaptation_value = survival_selection.survival_selection_function(mutated_population, parent_population)
+
+        #save the best adaptation value
+        best_adaptation_value[gen] = sorted_adaptation_value[0]
+
+        if abs(best_adaptation_value[gen - 1] - best_adaptation_value[gen - 2]) < constants.ERROR:
+            break
+
+        gen = gen + 1
 
     # print 2d plots to have an image of the reference functions
     if constants.PLOT_2D is True:
         statistics_plots.graphics_2d()
+
+    print(best_adaptation_value)
 
 if __name__ == "__main__":
     main()
--- survival_selection.py
+++ survival_selection.py
@@ -7,7 +7,6 @@ def survival_selection_function(mutated_vector, parent_vector):
 
     #initialize adaptation value
     adaptation_value = np.full(shape=(constants.POPULATION_SIZE + constants.OFFSPRING_SIZE, 2), fill_value=np.nan)
-    sorted_adaptation_value = np.full(shape=(constants.POPULATION_SIZE + constants.OFFSPRING_SIZE, 2), fill_value=np.nan)
 
     #evaluate mutated vector
     for i in range(constants.OFFSPRING_SIZE):
@@ -15,10 +14,10 @@ def survival_selection_function(mutated_vector, parent_vector):
         # save the index to support on the sort process
         adaptation_value[i, 1] = i
 
-        if constants.SHIFTED_SPH_FUN is True:
+        if constants.FUNCTION == 'SPHERE':
             adaptation_value[i, 0] = functions.shifted_sph_fun(mutated_vector[0:constants.DIM, i])
 
-        if constants.SCHWEFEL_FUN is True:
+        if constants.FUNCTION == 'SCHWEFEL':
             adaptation_value[i, 0] = functions.schwefel_fun(mutated_vector[0:constants.DIM, i])
 
     if constants.SELECTION_TYPE == 'NO_ELITISM':
@@ -26,19 +25,29 @@ def survival_selection_function(mutated_vector, parent_vector):
 
         survival_population = mutated_vector[:, sorted_adaptation_value[0:constants.POPULATION_SIZE, 1].astype(int)]
 
-        return survival_population
+        return survival_population, sorted_adaptation_value[0:constants.POPULATION_SIZE, 0]
 
     # evaluate parent vector
-    for i in range(constants.OFFSPRING_SIZE, constants.OFFSPRING_SIZE + constants.NUM_PARENTS, 1):
+    counter = 0
+    for i in range(constants.OFFSPRING_SIZE, constants.OFFSPRING_SIZE + constants.POPULATION_SIZE, 1):
 
         # save the index to support on the sort process
         adaptation_value[i, 1] = i
 
-        if constants.SHIFTED_SPH_FUN is True:
-            adaptation_value[i, 0] = functions.shifted_sph_fun(mutated_vector[0:constants.DIM - 1, i])
+        if constants.FUNCTION == 'SPHERE':
+            adaptation_value[i, 0] = functions.shifted_sph_fun(parent_vector[0:constants.DIM, counter])
 
-        if constants.SCHWEFEL_FUN is True:
-            adaptation_value[i, 0] = functions.schwefel_fun(mutated_vector[0:constants.DIM - 1, i])
+        if constants.FUNCTION == 'SCHWEFEL':
+            adaptation_value[i, 0] = functions.schwefel_fun(parent_vector[0:constants.DIM, i])
+
+        counter = counter + 1
 
     if constants.SELECTION_TYPE == 'ELITISM':
-        survival_vector = sorted()
+        sorted_adaptation_value = adaptation_value[np.argsort(adaptation_value[:, 0]), :]
+
+        #vector containing mutated vector and parent vector
+        elitist_vector = np.concatenate((mutated_vector, parent_vector),axis=1)
+
+        survival_population = elitist_vector[:, sorted_adaptation_value[0:constants.POPULATION_SIZE, 1].astype(int)]
+
+        return survival_population, sorted_adaptation_value[0:constants.POPULATION_SIZE, 0]
